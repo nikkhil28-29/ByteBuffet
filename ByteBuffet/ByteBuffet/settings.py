a5b39536a403e06895cbe1c867e9454d49b28ccd
Nikhil <its.nikhil2808@gmail.com>
2024-01-28 23:19:18 +0530

    added .env file

--- ByteBuffet/ByteBuffet/settings.py
+++ ByteBuffet/ByteBuffet/settings.py
@@ -67,7 +67,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', default=False, cast=bool) # from pypi python decouple
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 # Application definition
 
 INSTALLED_APPS = [
